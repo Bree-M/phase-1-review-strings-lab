16aeeac0806cea644bd57c256234307795cac40a
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-08 15:57:54 +0300

    updates

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
-const firstInitial = 'currentUser'[0];
-const restOfName = 'currentUser'.slice(1);
+
+const firstInitial = 'currentUser[0]';
+
+const restOfName = 'currentUser.slice(1)';
 
 const welcomeMessage = 'Welcome to Flatbook, + currentUser !';
 
