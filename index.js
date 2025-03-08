7d79773e9aace341ca2156b05788aac21106df35
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-08 16:31:46 +0300

    updates

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
 
-const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!;'
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
 
-const excitedWelcomeMessage = 'welcomemessage.toUpperCase(WELCOME TO FLATBOOK, + currentUser)!;'
+const excitedWelcomeMessage = welcomemessage.toUpperCase('WELCOME TO FLATBOOK, ${currentUser}')!
 
-const shortGreeting = 'Welcome, ${currentUser.slice(0)}!'
+const shortGreeting = 'Welcome, ${currentUser.slice(0,1)}!';
